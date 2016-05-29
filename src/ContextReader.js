be1bf8f716037fba13bac6d15d2a4ea5c7dc2eee
Ryan Graff <ryan@StellarOmega.local>
2016-05-28 23:18:17 -0400

    feat() Add initial test setup.

---
+++ src/ContextReader.js
@@ -0,0 +1,14 @@
+import Glob from 'glob';
+import Path from 'path';
+import FS from 'fs';
+
+export default class ContextReader {
+  static readContexts(paths){
+    const context = {};
+
+    return context;
+  }
+
+  constructor() {
+  }
+}
